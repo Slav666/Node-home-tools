b3112e4a2a2509dea59e82c074b5abd0f5b7cfeb
Slawomir Dyk <slawomir.dyk@astrosat.net>
2024-03-22 10:15:54 +0000

    work on a server logic

--- server.js
+++ server.js
@@ -5,49 +5,63 @@ const fsPromises = require("fs").promises;
 
 const logEvents = require("./logEvents");
 const EventEmitter = require("events");
-
 class Emitter extends EventEmitter {}
-
+// initialize object
 const myEmitter = new Emitter();
-
+myEmitter.on("log", (msg, fileName) => logEvents(msg, fileName));
 const PORT = process.env.PORT || 3500;
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+  myEmitter.emit("log", `${req.url}\t${req.method}`, "reqLog.txt");
+
+  const extension = path.extname(req.url);
+
+  let contentType;
+
+  switch (extension) {
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".jpg":
+      contentType = "image/jpeg";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".txt":
+      contentType = "text/plain";
+      break;
+    default:
+      contentType = "text/html";
+  }
+
+  let filePath =
+    contentType === "text/html" && req.url === "/"
+      ? path.join(__dirname, "views", "index.html")
+      : contentType === "text/html" && req.url.slice(-1) === "/"
+      ? path.join(__dirname, "views", req.url, "index.html")
+      : contentType === "text/html"
+      ? path.join(__dirname, "views", req.url)
+      : path.join(__dirname, req.url);
+
+  // makes .html extension not required in the browser
+  if (!extension && req.url.slice(-1) !== "/") filePath += ".html";
+
+  const fileExists = fs.existsSync(filePath);
+
+  if (fileExists) {
+    //serve the file
+  } else {
+    // 404
+    //301 redirect
+    console.log(path.parse(filePath));
+  }
 });
-
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
- const extension = path.extname(req.url);
-
- let contentType;
-
- switch (extension) {
-   case ".css":
-     contentType = "text/css";
-     break;
-   case ".js":
-     contentType = "text/javascript";
-     break;
-   case ".json":
-     contentType = "application/json";
-     break;
-   case ".jpg":
-     contentType = "image/jpeg";
-     break;
-   case ".png":
-     contentType = "image/png";
-     break;
-   case ".txt":
-     contentType = "text/plain";
-     break;
-   default:
-     contentType = "text/html";
- }
-
-
-// myEmitter.on("log", (msg) => logEvents(msg));
-
-// setTimeout(() => {
-//   myEmitter.emit("log", "log event emitted");
-// }, 2000);
